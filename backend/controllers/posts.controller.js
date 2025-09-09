352873a26276fe43fda1ab0c0b642ae4e3fff232
vikas jogi <jogivikas666@gmail.com>
2025-09-09 18:40:50 +0530

    createpost

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,5 +1,37 @@
+import Post from "../models/posts.model.js";
+import User from "../models/user.model.js";
+import Profiles from "../models/profiles.model.js";
+import bcrypt from "bcrypt";
+import { Router } from "express";
+
 export const activeCheck = async (req, res) => {
   return res.status(200).json({
     message: "RUNNING",
   });
+};
+
+export const createPost = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const post = new Post({
+      userId: user._id,
+      body: req.body.body,
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+    await post.save();
+    return res.status(200).json({
+      message: "Post created successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -38,6 +38,7 @@ router.route("/user_update").post(updateUserProfile);
 router.route("/get_user_and_profile").get(getUserAndProfile);
 
 router.route("/update_profile_data").post(updateProfileData);
+
 router.route("/user/get_all_users").get(getAllUserProfile);
 router.route("/user/download_resume").get(downloadResume);
 
